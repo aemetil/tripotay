e76f8cca4fea24ddf2486e43dc7817a7fd4c9460
aemetil <e.aemetil@gmail.com>
2026-03-27 03:57:38 +0100

    fix: augmente max_length du champ icon Category 10=>50 pour PostgreSQL

--- apps/categories/models.py
+++ apps/categories/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, unique=True)
     description = models.TextField(blank=True, default='')
-    icon = models.CharField(max_length=10, blank=True, default='')  # emoji
+    icon = models.CharField(max_length=50, blank=True, default='')  # Material Icons name
     order = models.PositiveSmallIntegerField(default=0)
 
     class Meta:
